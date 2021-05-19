eadeef59c49d202ef38e2f8819157f7da0e9f69d
laz <danier123@gmail.com>
2021-05-19 17:47:44 +0900

    feat: chatroom 모델 생성

---
+++ lib/model/Chatroom.dart
@@ -0,0 +1,12 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Chatroom {
+  Timestamp start;
+  Timestamp end;
+  String tutorName;
+  String status;
+  String zoomLink;
+  String chats;
+  String participants;
+}
