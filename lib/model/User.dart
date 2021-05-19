df3e352b7b4ea84dfb032e69b8c6e248a424787f
laz <danier123@gmail.com>
2021-05-19 17:59:57 +0900

    feat: user 모델 업데이트

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -1,9 +1,18 @@
 
+import 'package:flutter/material.dart';
+
 class User {
-  String name;
-  int studentId;
-  String email;
-  int time;
-  String type;
-  // ref to session sub collection
+  final String name;
+  final int studentId;
+  final String email;
+  final int time;
+  final String type;
+
+  User({
+    @required this.name,
+    @required this.studentId,
+    @required this.email,
+    @required this.time,
+    @required this.type,
+  });
 }
