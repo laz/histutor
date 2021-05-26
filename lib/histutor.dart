967546c7e67c34bf41ef081aefa820c4cb196572
exponent112 <21800181@handong.edu>
2021-05-26 14:15:17 +0900

    _2

--- lib/histutor.dart
+++ lib/histutor.dart
@@ -2,11 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:histutor/state/ApplicationState.dart';
 
 
+class histutor extends StatelessWidget {
   const histutor({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('hitutor')
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('body')
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.wb_sunny),
+          onPressed: () {
+            ApplicationState applicationState = ApplicationState();
+            applicationState.addParticipant();
+          },
+        ),
       ),
     );
   }
