321e50da2dcc8a9481f4bc8aaeefbb7c6e7728a4
exponent112 <21800181@handong.edu>
2021-05-26 14:22:28 +0900

    _3

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:histutor/histutor.dart';
 import 'package:histutor/state/ApplicationState.dart';
 import 'package:histutor/state/Authentication.dart';
-import 'package:histutor/temp.dart';
-import 'package:provider/provider.dart';
 
-import 'makeRoom.dart';
-
-void main() {
-  runApp(
-    ChangeNotifierProvider<Authentication>(
-      create: (context) => Authentication(),
-      child: ChangeNotifierProvider<ApplicationState>(
-        create: (context) => ApplicationState(),
-        child: temp()
-        //makeRoom(),
-        //temp(),
-      ),
-    )
-  );
-}
 
