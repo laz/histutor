326b9366046fc59157cac6b958eb900d706e59e5
kimyouyoung <21800147@handong.edu>
2021-06-03 10:00:32 +0900

    modify chat

--- lib/Chatting.dart
+++ lib/Chatting.dart
@@ -50,10 +50,8 @@ class ChatMessage extends StatelessWidget {
 
 class Chatting extends StatefulWidget {
   final int sessionIndex;
-  final String fromName;
 
-  Chatting({Key key, @required this.sessionIndex, @required this.fromName})
-      : super(key: key);
+  Chatting({Key key, @required this.sessionIndex}) : super(key: key);
 
   @override
   _ChattingState createState() => _ChattingState(this.sessionIndex);
@@ -75,139 +73,147 @@ class _ChattingState extends State<Chatting> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     List<Chat> chats = Provider.of<List<Chat>>(context);
     List<Participant> participants = Provider.of<List<Participant>>(context);
-    List<Session> sessions = Provider.of<List<Session>>(context);
     Session session = Provider.of<Session>(context);
     User auth = Provider.of<User>(context);
 
-    studentId = auth.studentId.toString();
-    name = auth.name;
+    if (auth != null) studentId = auth.studentId.toString();
+    if (auth != null) name = auth.name;
 
-    return session.category.compareTo('종료') == 1
-        ? Scaffold(
-            appBar: AppBar(
-              title: Text('Chats'),
-            ),
-            body: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.fromLTRB(150.0, 50.0, 0.0, 50.0),
-                  width: 500.0,
-                  height: 700.0,
-                  child: Column(
-                    children: [
-                      Flexible(
-                        child: chats != null
-                            ? ListView.builder(
-                                itemCount: chats.length,
-                                itemBuilder: (context, index) {
-                                  return ListTile(
-                                    title: Text(chats[index].from),
-                                    subtitle: Text(chats[index].text),
-                                  );
-                                },
-                              )
-                            : CircularProgressIndicator(),
-                      ),
-                      Divider(height: 1.0),
-                      Container(child: _buildTextComposer()),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 20.0),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0.0, 50.0, 100.0, 50.0),
-                  width: 400.0,
-                  height: MediaQuery.of(context).size.height,
-                  child: session != null
-                      ? Column(
-                          children: [
-                            Text('SESSION NAME: ' + session.sessionName),
-                            Text('TUTOR: ' + session.tutorName),
-                            Text('OFFLINE: ' + session.offline),
-                            Text('START TIME: ' +
-                                '${session.sessionStart.toDate().toString()}'),
-                            Text('ZOOM LINK: ' + session.zoomLink),
-                            Row(
-                              children: [
-                                Text('TUTEES: '),
-                                participants != null
-                                    ? Column(
-                                        children: [
-                                          for (var p in participants)
-                                              Text(p.name)
-                                        ],
-                                      )
-                                    : CircularProgressIndicator(),
-                              ],
-                            ),
-                            SizedBox(height: 50.0),
-                            if (auth.Uid == session.tutorUid)
-                              _buildTutorButton(participants[0], session),
-
-                            Container(
-                              width: 300,
-                              child: ElevatedButton(
-                                onPressed: () async {
-                                  await FirebaseFirestore.instance.collection('Sessions')
-                                  .doc(sessionIndex.toString()).collection('Participants')
-                                  .doc(auth.studentId.toString()).delete();
-                                  Navigator.of(context).pop();
-                                },
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                  MaterialStateProperty.all(Color(0xff9BC7DA)),
-                                ),
-                                child: Text(
-                                  "나가기",
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                      : CircularProgressIndicator(),
-                )
-              ],
-            ),
-          )
-        : Dialog(
-            child: Container(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('해당 세션은 종료되었습니다:('),
-                SizedBox(height: 30.0),
-                Container(
-                  width: 300,
-                  child: ElevatedButton(
+    return session != null
+        ? session.category.compareTo('종료') == 1
+            ? Scaffold(
+                appBar: AppBar(
+                  title: Text('Chats'),
+                  leading: IconButton(
                     onPressed: () async {
+                      if (auth.Uid != session.tutorUid)
+                        await FirebaseFirestore.instance
+                            .collection('Sessions')
+                            .doc(sessionIndex.toString())
+                            .collection('Participants')
+                            .doc(auth.studentId.toString())
+                            .delete();
+
+                      if(auth.Uid != session.tutorUid)
+                        await FirebaseFirestore.instance
+                        .collection('Users')
+                        .doc(auth.studentId.toString())
+                        .collection('Sessions')
+                        .add({
+                          'date': FieldValue.serverTimestamp(),
+                          'sessionName': session.sessionName,
+                          'time': 0,
+                          'tutorName': session.tutorName,
+                        });
+
                       Navigator.of(context).pop();
                     },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Color(0xff9BC7DA)),
-                    ),
-                    child: Text(
-                      "세션 목록으로 돌아가기",
-                    ),
+                    icon: Icon(Icons.arrow_back),
                   ),
                 ),
-              ],
-            )),
-          );
+                body: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(150.0, 50.0, 0.0, 50.0),
+                      width: 500.0,
+                      height: 700.0,
+                      child: Column(
+                        children: [
+                          Flexible(
+                            child: chats != null
+                                ? ListView.builder(
+                                    itemCount: chats.length,
+                                    itemBuilder: (context, index) {
+                                      return ListTile(
+                                        title: Text(chats[index].from),
+                                        subtitle: Text(chats[index].text),
+                                      );
+                                    },
+                                  )
+                                : CircularProgressIndicator(),
+                          ),
+                          Divider(height: 1.0),
+                          Container(child: _buildTextComposer()),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20.0),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0.0, 50.0, 100.0, 50.0),
+                      width: 400.0,
+                      height: MediaQuery.of(context).size.height,
+                      child: session != null
+                          ? Column(
+                              children: [
+                                Text('SESSION NAME: ' + session.sessionName),
+                                Text('TUTOR: ' + session.tutorName),
+                                Text('OFFLINE: ' + session.offline),
+                                Text('START TIME: ' +
+                                    '${session.sessionStart.toDate().toString()}'),
+                                Text('ZOOM LINK: ' + session.zoomLink),
+                                Row(
+                                  children: [
+                                    Text('TUTEES: '),
+                                    participants != null
+                                        ? Column(
+                                            children: [
+                                              for (var p in participants)
+                                                Text(p.name)
+                                            ],
+                                          )
+                                        : CircularProgressIndicator(),
+                                  ],
+                                ),
+                                SizedBox(height: 50.0),
+                                if (session != null)
+                                  _buildTutorButton(participants, session),
+                              ],
+                            )
+                          : CircularProgressIndicator(),
+                    )
+                  ],
+                ),
+              )
+            : Dialog(
+                child: Container(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('해당 세션은 종료되었습니다:('),
+                    SizedBox(height: 30.0),
+                    Container(
+                      width: 300,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          Navigator.of(context).pop();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Color(0xff9BC7DA)),
+                        ),
+                        child: Text(
+                          "세션 목록으로 돌아가기",
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+              )
+        : CircularProgressIndicator();
   }
 
-  Widget _buildTutorButton(Participant p, Session session) {
+  Widget _buildTutorButton(List<Participant> ps, Session session) {
     return Column(
       children: [
         Container(
           width: 100,
           child: ElevatedButton(
             onPressed: () {
-              if (p.uid != session.tutorUid)
-                addChatMessage(p.name + '님 차례입니다:)', p.name);
+              if (ps != null) if (ps.length > 0)
+                addChatMessage(ps[0].name + '님 차례입니다:)');
               else
-                addChatMessage('현재 진행할 튜티가 없습니다:(', p.name);
+                addChatMessage('현재 진행할 튜티가 없습니다:(');
             },
             style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(Color(0xff9BC7DA)),
@@ -248,11 +254,6 @@ class _ChattingState extends State<Chatting> with TickerProviderStateMixin {
                   .update({
                 'category': "종료",
               });
-              Navigator.of(context).pop();
-              // Navigator.of(context).push(
-              //   MaterialPageRoute(
-              //       builder: (context) => SessionPage()),
-              // );
             },
             style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(Color(0xff9BC7DA)),
@@ -274,12 +275,12 @@ class _ChattingState extends State<Chatting> with TickerProviderStateMixin {
           Flexible(
             child: TextField(
               controller: _textController,
+              onSubmitted: _isComposing ? _handleSubmitted : null,
               onChanged: (String text) {
                 setState(() {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              // onSubmitted: _isComposing ? _handleSubmitted : null,
               decoration: InputDecoration.collapsed(hintText: 'Send a message'),
               focusNode: _focusNode,
             ),
@@ -288,7 +289,7 @@ class _ChattingState extends State<Chatting> with TickerProviderStateMixin {
               margin: EdgeInsets.symmetric(horizontal: 4.0),
               child: CupertinoButton(
                 onPressed: _isComposing
-                    ? () => _handleSubmitted(_textController.text, name)
+                    ? () => _handleSubmitted(_textController.text)
                     : null,
                 child: Icon(Icons.send),
               ))
@@ -297,7 +298,7 @@ class _ChattingState extends State<Chatting> with TickerProviderStateMixin {
     );
   }
 
-  void _handleSubmitted(String text, String name) {
+  void _handleSubmitted(String text) {
     _textController.clear();
     setState(() {
       _isComposing = false;
@@ -311,13 +312,13 @@ class _ChattingState extends State<Chatting> with TickerProviderStateMixin {
       ),
     );
     setState(() {
-      addChatMessage(text, name);
+      addChatMessage(text);
     });
     _focusNode.requestFocus();
     message.animationController.forward();
   }
 
-  Future<DocumentReference> addChatMessage(String text, String name) {
+  Future<DocumentReference> addChatMessage(String text) {
     return FirebaseFirestore.instance
         .collection('Sessions')
         .doc(sessionIndex.toString())
