badd6031eacca14ff541176ed39e923b206a039c
laz <danier123@gmail.com>
2021-09-11 23:01:51 +0900

    feat: 현재 시간을 기준으로 진행중과 진행 예정 구분

--- lib/screens/MyPageWidgets/user/list/Session.dart
+++ lib/screens/MyPageWidgets/user/list/Session.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:histutor/screens/MyPageWidgets/user/list/Chatting.dart';
 import 'package:histutor/model/Chat.dart';
@@ -13,111 +12,132 @@ import '../../../../model/Participant.dart';
 import '../../../../state/Database.dart';
 
 class Sessions extends StatelessWidget {
-  const Sessions({Key key, @required this.idx, @required this.sessions}) : super(key: key);
+  const Sessions({Key key, @required this.idx, @required this.sessions})
+      : super(key: key);
   final int idx;
   final List<Session> sessions;
 
   @override
   Widget build(BuildContext context) {
     User user = Provider.of<User>(context);
-
-    return Row(
-      children: [
-        Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.only(left: 30),
-          width: 80,
-          child: Text(sessions[idx].sessionIndex.toString(),
-              style: TextStyle(
-                fontSize: 15,
-              )),
-        ),
-        Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.only(right: 30),
-          width: 120,
-          child: Text(sessions[idx].category,
-              style: TextStyle(
-                fontSize: 15,
-              )),
-        ),
-        Expanded(
-          // padding: EdgeInsets.only(left: 50),
-          child: Text(sessions[idx].sessionName,
-              style: TextStyle(
-                fontSize: 15,
-              )),
-        ),
-        Container(
-          alignment: Alignment.center,
-          width: 100,
-          // padding: EdgeInsets.only(left: 50),
-          child: Text(sessions[idx].tutorName,
-              style: TextStyle(
-                fontSize: 15,
-              )),
-        ),
-        Container(
-          alignment: Alignment.center,
-          width: 100,
-          child: Column(
+    return StreamBuilder(
+        stream: Stream.periodic(const Duration(seconds: 1)),
+        builder: (context, snapshot) {
+          return Row(
             children: [
-              Text(DateFormat('yyyy-MM-dd').format(sessions[idx].sessionStart.toDate()).toString()),
-              Text(
-                DateFormat('HH:mm').format(sessions[idx].sessionStart.toDate()).toString() +
-                    '-' +
-                    DateFormat('HH:mm').format(sessions[idx].sessionEnd.toDate()).toString(),
-                style: TextStyle(color: Colors.grey),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: Container(
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () async {
-
-                  if(sessions[idx].participants.contains(user.id)) {
-                    // if tutoring
-                    if(sessions[idx].actualStudentBeingTutored == user.id) {
-                      // do nothing
-                    }
-                    else {
-                      SessionController().updateEntranceTime(user, sessions[idx].sessionIndex.toString());
-                    }
-                  }
-                  else {
-
-                    if(user.id != sessions[idx].tutorId) {
-                      // 넣기
-                      SessionController().addParticipant(user, sessions[idx].sessionIndex.toString());
-                      SessionController().addSessionParticipant(user, sessions[idx].sessionIndex.toString());
-                    }
-                  }
-
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                    return MultiProvider(
-                      providers: [
-                        StreamProvider<List<Chat>>.value(value: Database().getSessionChats(sessions[idx].sessionIndex)),
-                        StreamProvider<Session>.value(value: Database().getSession(sessions[idx].sessionIndex)),
-                        StreamProvider<List<Participant>>.value(value: Database().getSessionParticipants(sessions[idx].sessionIndex)),
-                      ],
-                      child: Chatting(sessionIndex: sessions[idx].sessionIndex),
-                    );
-                  }));
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Color(0xff9BC7DA)),
-                ),
-                child: Text("입  장",
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 30),
+                width: 80,
+                child: Text(sessions[idx].sessionIndex.toString(),
                     style: TextStyle(
                       fontSize: 15,
                     )),
               ),
-            )),
-      ],
-    );
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(right: 30),
+                width: 120,
+                child: Text(
+                    DateTime.now().isBefore(sessions[idx].sessionStart.toDate())
+                        ? '진행 예정'
+                        : '진행중',
+                    style: TextStyle(
+                      fontSize: 15,
+                    )),
+              ),
+              Expanded(
+                // padding: EdgeInsets.only(left: 50),
+                child: Text(sessions[idx].sessionName,
+                    style: TextStyle(
+                      fontSize: 15,
+                    )),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 100,
+                child: Text(sessions[idx].tutorName,
+                    style: TextStyle(
+                      fontSize: 15,
+                    )),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 100,
+                child: Column(
+                  children: [
+                    Text(DateFormat('yyyy-MM-dd')
+                        .format(sessions[idx].sessionStart.toDate())
+                        .toString()),
+                    Text(
+                      DateFormat('HH:mm')
+                              .format(sessions[idx].sessionStart.toDate())
+                              .toString() +
+                          '-' +
+                          DateFormat('HH:mm')
+                              .format(sessions[idx].sessionEnd.toDate())
+                              .toString(),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: Container(
+                    width: 100,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (sessions[idx].participants.contains(user.id)) {
+                          // if tutoring
+                          if (sessions[idx].actualStudentBeingTutored ==
+                              user.id) {
+                            // do nothing
+                          } else {
+                            SessionController().updateEntranceTime(
+                                user, sessions[idx].sessionIndex.toString());
+                          }
+                        } else {
+                          if (user.id != sessions[idx].tutorId) {
+                            // 넣기
+                            SessionController().addParticipant(
+                                user, sessions[idx].sessionIndex.toString());
+                            SessionController().addSessionParticipant(
+                                user, sessions[idx].sessionIndex.toString());
+                          }
+                        }
+
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return MultiProvider(
+                            providers: [
+                              StreamProvider<List<Chat>>.value(
+                                  value: Database().getSessionChats(
+                                      sessions[idx].sessionIndex)),
+                              StreamProvider<Session>.value(
+                                  value: Database()
+                                      .getSession(sessions[idx].sessionIndex)),
+                              StreamProvider<List<Participant>>.value(
+                                  value: Database().getSessionParticipants(
+                                      sessions[idx].sessionIndex)),
+                            ],
+                            child: Chatting(
+                                sessionIndex: sessions[idx].sessionIndex),
+                          );
+                        }));
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Color(0xff9BC7DA)),
+                      ),
+                      child: Text("입  장",
+                          style: TextStyle(
+                            fontSize: 15,
+                          )),
+                    ),
+                  )),
+            ],
+          );
+        });
   }
 }
